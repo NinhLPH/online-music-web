5807d81b4f1b3222fb98549db9792fc43085f0ab
phuctbhe182177 <phuctbhe182177@fpt.edu.vn>
2025-10-23 16:58:53 +0700

    add new Navbar component and restructure Header

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,35 +1,49 @@
 import React from "react";
-import "./Navbar.css";
 import { FaBell, FaCog, FaUser } from "react-icons/fa";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 export default function Navbar() {
   return (
-    <nav className="navbar">
-      {/* Logo */}
-      <div className="navbar-logo">
-        <img
-          src="https://a-v2.sndcdn.com/assets/images/sc-icons/ios-a62dfc8bdb.png"
-          alt="Logo"
-        />
-      </div>
+    <nav className="navbar navbar-expand-lg bg-dark navbar-dark px-3">
+      <div className="container-fluid d-flex align-items-center justify-content-between">
 
-      {/* Menu */}
-      <ul className="navbar-menu">
-        <li>Home</li>
-        <li>Library</li>
-        <li>Favourite</li>
-      </ul>
+        {/* Logo */}
+        <div className="d-flex align-items-center">
+          <img
+            src="https://a-v2.sndcdn.com/assets/images/sc-icons/ios-a62dfc8bdb.png"
+            alt="Logo"
+            style={{ height: "40px" }}
+          />
+        </div>
 
-      {/* Search */}
-      <div className="navbar-search">
-        <input type="text" placeholder="Search" />
-      </div>
+        {/* Menu */}
+        <ul className="navbar-nav d-flex flex-row mx-3">
+          <li className="nav-item mx-2">
+            <a href="#" className="nav-link text-white">Home</a>
+          </li>
+          <li className="nav-item mx-2">
+            <a href="#" className="nav-link text-white">Library</a>
+          </li>
+          <li className="nav-item mx-2">
+            <a href="#" className="nav-link text-white">Favourite</a>
+          </li>
+        </ul>
 
-      {/* Icons */}
-      <div className="navbar-icons">
-        <FaBell className="icon" />
-        <FaCog className="icon" />
-        <FaUser className="icon" />
+        {/* Search */}
+        <div className="flex-grow-1 mx-3" style={{ maxWidth: "800px" }}>
+          <input
+            type="text"
+            className="form-control"
+            placeholder="Search"
+          />
+        </div>
+
+        {/* Icons */}
+        <div className="d-flex align-items-center text-white">
+          <FaBell className="mx-2 fs-5" />
+          <FaCog className="mx-2 fs-5" />
+          <FaUser className="mx-2 fs-5" />
+        </div>
       </div>
     </nav>
   );
