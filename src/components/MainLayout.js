8f69291080468e9aa756c8caf1d2742b25e081f4
phucbaehe186911-boop <phucbaehe186911@gmail.com>
2025-10-23 17:25:53 +0700

    MainLayout.js

--- src/components/MainLayout.js
+++ src/components/MainLayout.js
@@ -1,14 +1,21 @@
-import React from 'react';
-import { Outlet } from 'react-router-dom';
-import Header from './Header';
-import LeftSidebar from './LeftSidebar';
-import RightSidebar from './RightSidebar';
-import PlayerBar from './PlayerBar';
+import React, { useState } from "react";
+import { Outlet } from "react-router-dom";
+import Header from "./Header";
+import LeftSidebar from "./LeftSidebar";
+import RightSidebar from "./RightSidebar";
+import PlayerBar from "./PlayerBar";
 
-const HEADER_HEIGHT = '70px';
-const PLAYER_BAR_HEIGHT = '90px';
+const HEADER_HEIGHT = "70px";
+const PLAYER_BAR_HEIGHT = "90px";
 
 function MainLayout() {
+  const [selectedPlaylist, setSelectedPlaylist] = useState(null);
+
+  const handleSelectPlaylist = (playlist) => {
+    console.log("Selected playlist:", playlist);
+    setSelectedPlaylist(playlist);
+  };
+
   return (
     <div className="d-flex flex-column vh-100">
       <div style={{ height: HEADER_HEIGHT }}>
@@ -16,28 +23,32 @@ function MainLayout() {
       </div>
 
       <div className="flex-grow-1 overflow-hidden row g-0">
-        
-        {/*Library, playlist,..*/}
+        {/* Sidebar */}
         <div className="col-2 bg-light border-end overflow-auto">
-          <LeftSidebar />
+          <LeftSidebar onSelectPlaylist={handleSelectPlaylist} />
         </div>
 
-        {/*Main content*/}
+        {/* Main Content */}
         <main className="col-8 overflow-auto">
           <div className="p-3">
-            {/*home, search, song,...
-            */}
-            <Outlet /> 
+            {selectedPlaylist ? (
+              <div>
+                <h3>{selectedPlaylist.name}</h3>
+                <p>{selectedPlaylist.description}</p>
+                <p>(Hiển thị danh sách bài hát ở đây)</p>
+              </div>
+            ) : (
+              <Outlet />
+            )}
           </div>
         </main>
 
-        {/*songcard*/}
+        {/* Right Sidebar */}
         <div className="col-2 bg-light border-start overflow-auto">
           <RightSidebar />
         </div>
       </div>
 
-      {/*Player bar*/}
       <div style={{ height: PLAYER_BAR_HEIGHT }}>
         <PlayerBar />
       </div>
